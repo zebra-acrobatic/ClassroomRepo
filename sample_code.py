9080b97dce5334495dd01d0692a7a60c719cdacc
RYLEY ROBINSON <ROBINR@tdm.local>
2024-02-08 16:14:56 +0800

    Changed numbers.

--- sample_code.py
+++ sample_code.py
@@ -3,16 +3,16 @@ Sample Code for class
 '''
 import time
 
-# Ask the user for input between 1 and 3
-user_input = input("Enter a number between 1 and 3: ")
+# Ask the user for input between 0 and 2
+user_input = input("Enter a number between 0 and 2: ")
 
 # Check user input and provide corresponding output
-if user_input == '1':
+if user_input == '0':
     print("You chose 1. Something interesting could be said here.")
-elif user_input == '2':
+elif user_input == '1':
     print("You chose 2. Here's something else for you.")
-elif user_input == '3':
+elif user_input == '2':
     current_time = time.strftime("%H:%M:%S", time.localtime())
-    print(f"You chose 3. The current time is {current_time}.")
+    print(f"You chose 2. The current time is {current_time}.")
 else:
-    print("Invalid input. Please enter a number between 1 and 3.")
+    print("Invalid input. Please enter a number between 0 and 2.")
